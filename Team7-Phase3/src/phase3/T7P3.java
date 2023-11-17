8013ff5825b29fd64a05d5f4490c9fe987b11576
minguk-cucu <74689888+minguk-cucu@users.noreply.github.com>
2023-11-17 20:10:22 +0900

    minguk's initial

---
+++ Team7-Phase3/src/phase3/T7P3.java
@@ -0,0 +1,82 @@
+package phase3;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class T7P3 {	
+	public static void main(String[] args) {
+		/////CUSTOMIZE THIS
+		String serverIP = "localhost";
+		String strSID = "orcl";
+		String portNum = "1521";
+		String user = "minguk";
+		String pass = "0118";
+		String url = "jdbc:oracle:thin:@"+serverIP+":"+portNum+":"+strSID;
+		/////
+		Connection conn = null;
+		ResultSet rs = null;
+		Statement stmt = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(url,user,pass);
+			stmt = conn.createStatement();
+			
+			conn.setAutoCommit(false);
+			
+			Initial.initialInterface(conn, stmt);
+			
+			if( stmt != null ) stmt.close();
+			if( conn != null ) conn.close();
+			if( rs != null ) rs.close();
+//			if( pstmt != null ) pstmt.close();
+	
+		}catch (ClassNotFoundException e){
+			e.printStackTrace();
+			System.err.println("Cannot Load OracleDriver");
+			System.exit(-1);
+		}catch ( SQLException ex ) {
+			ex.printStackTrace();
+			System.err.println("SQL Error");
+			System.exit(-1);
+		}
+	
+	}
+	
+	public static void menuList(Connection conn, Statement stmt) throws SQLException{
+		Scanner sc = new Scanner(System.in);
+		int ch;
+		
+		while(true) {
+
+			while(true) {
+				System.out.println("Which feature do you want to Use ?");
+				System.out.println("0. About User 1. Evaluation 2. Match . . . . . . . 추가하기");
+				System.out.print(": ");
+				try{
+					ch = sc.nextInt();
+					break;
+				}catch(java.util.InputMismatchException e) {
+					System.out.println("You should give proper input");
+					sc.nextLine();
+				}			
+			}
+			
+			switch(ch) {
+			case 0:
+				User.userInfo(conn, stmt);
+				break;
+			case 1:
+				Evaluation.evaluationMenu(conn, stmt);
+				break;
+			default:
+				System.exit(0);
+			}
+	
+		}
+
+	}
+	
+
+}
---
+++ Team7-Phase3/src/phase3/User.java
@@ -0,0 +1,100 @@
+package phase3;
+
+import java.sql.*;
+import java.util.Scanner;
+
+
+public class User {
+	static int user_id = -1;
+	
+	public static void userInfo(Connection conn, Statement stmt) throws SQLException {
+		if( user_id == -1 ) {
+			System.err.println("illegal user");
+			System.exit(-1);
+		}
+		ResultSet rs = null;
+		Scanner sc = new Scanner(System.in);
+		int ch;
+		String sql = "SELECT u.name, t.team_name FROM USER_INFO u, USER_FOLLOW_TEAM t "
+				+ "WHERE u.user_id = t.user_id "
+				+ "AND u.user_id = " + user_id;
+
+		rs = stmt.executeQuery(sql);
+		if(rs.next()) {
+			String user = rs.getString(1);
+			while(true) {
+				
+				System.out.println("This is your user page.");
+				System.out.print("Your name is : " + user);
+				System.out.println(" , and You're following : " + rs.getString(2));
+				System.out.println("1.Delete your account 2. Go back");
+				System.out.print(": ");
+				try{
+					ch = sc.nextInt();
+					break;
+				}catch(java.util.InputMismatchException e) {
+					System.out.println("You should give proper input");
+					sc.nextLine();
+				}			
+			}
+			
+			switch(ch){
+			case 1:
+				deleteUserInfo(conn,stmt,user);
+			default:
+				break;
+			}
+		}
+		else {
+			System.err.println("We cannot find user having your id. Who are you .. ?");
+			System.exit(-1);
+		}
+		
+		
+		rs.close();
+
+	}
+	
+	public static void deleteUserInfo(Connection conn, Statement stmt, String user) throws SQLException{
+		Scanner sc = new Scanner(System.in);
+		int ch;
+		
+		while(true) {
+			System.out.println("Are You SURE to delete this account ? : "+ user);
+			System.out.println("1. Yes 2. No ");
+			System.out.print(": ");
+			try{
+				ch = sc.nextInt();
+				break;
+			}catch(java.util.InputMismatchException e) {
+				System.out.println("You should give proper input");
+				sc.nextLine();
+			}		
+
+		}
+		
+		switch(ch) {
+		case 1:
+			String sql = "DELETE FROM USER_INFO " +
+						 "WHERE user_id = " + user_id;
+			int result = stmt.executeUpdate(sql);
+			if(result == 1 ) {
+				System.out.println("Deletion complete .");
+//				conn.commit();
+				System.exit(0);
+			}
+			else {
+				System.err.println("Something went wrong deleting your account from DB");
+				System.exit(-1);
+			}
+			return;
+		case 2:
+			return;
+		default:
+			return;
+		}
+		
+		
+		
+	}
+}
